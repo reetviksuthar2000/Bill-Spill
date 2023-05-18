3c4718d4145de299baaec872ea917af9b1584693
reetviksuthar2000 <107484724+reetviksuthar2000@users.noreply.github.com>
2023-05-18 12:23:17 +0530

    Update script.js

--- Bill Spill/script.js
+++ Bill Spill/script.js
@@ -8,37 +8,73 @@ const decrease = document.querySelector(".b9");
 const generate = document.querySelector(".b10");
 const reset = document.querySelector(".b11");
 
-var value =0;
-var number = 0;
-var pecentage_tip = 0;
+let value = 0;
+let peopleVal = 1;
+let tipVal = 0;
 // console.log(bill);
 
-bill.addEventListener("input", (e) => {
-    value = parseInt(e.target.value);
-    if (value < 0 || !Boolean(value)) {
-      
-      value = 0;
-      
-    } else {
-      
-      
-      tipAmount();
-      totalF();
-      resetF();
+// increase and decrease button
+peopleVal= parseInt(people.innerText = peopleVal);
+document.querySelector(".b7").addEventListener('click', function () {
+    peopleVal = peopleVal + 1;
+    people.innerText = peopleVal;
+})
+
+document.querySelector('.b9').addEventListener('click', function () {
+    peopleVal = --peopleVal;
+    if(peopleVal < 0){
+        peopleVal = 0;
     }
+    people.innerText = peopleVal;
+})
+
+
+//add event listner
+bill.addEventListener('input', (e) =>{
+    value = parseFloat(e.traget.value);
+    calculate();
     console.log(bill.value);
-  });
+});
 
-const tipAmount = () => {
-    const tip_person = ((value * (percentage__tip / 100)) / number).toFixed(2);
-    console.log(tip_person);
-    tip__display.innerText = `₹{tip_person}`;
-};
-const totalF = () => {
-    var total_person = (
-      (value + value * (percentage__tip / 100)) /
-      number
-    ).toFixed(2);
-    total.innerText = `₹{total_person}`;
+tip.forEach(btn => {
+    btn.addEventListener('click',handleClick)
+});
+
+generate.addEventListener('click', generatefn)
+
+reset.addEventListener('click' , handleReset)
+
+//handle click function
+function handleClick(event){
+    tip.forEach(btn => {
+        btn.classList.remove('active')
+        if(event.target.innerHTML === btn.innerHTML){
+            btn.classList.add('active');
+            tipVal = parseFloat(btn.innerHTML)/100
+        }
+    })
+    
+    calculate()
+}
+// calculate funtion
+function calculate() {
+    if(peopleVal >= 1 ) {
+        let tip = billVal * tipVal / peopleVal;
+        let totalAmount = billVal * (tipVal + 1) / peopleVal;
+
+        totalVal[0].innerHTML = '₹' + tip.toFixed(2);
+        totalVal[1].innerHTML = '₹' + totalAmount.toFixed(2);
+    }
+}
+
+
+ function generatefn () {
+    
+    calculate() ;
 };
 
+function handleReset(){
+    people.innerText = '0' ;
+    totalVal[0].innerHTML = '0';
+    totalVal[1].innerHTML = '0';
+}
